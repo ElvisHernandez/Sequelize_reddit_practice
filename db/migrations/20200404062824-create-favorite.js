0bc0b1c542cb533803fc4c6978a252904478127a
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-04 02:30:35 -0400

    added favorites model

--- db/migrations/20200404062824-create-favorite.js
+++ db/migrations/20200404062824-create-favorite.js
@@ -11,6 +11,9 @@ module.exports = {
       title: {
         type: Sequelize.STRING
       },
+      thumbnail: {
+        type: Sequelize.STRING
+      },
       upvotes: {
         type: Sequelize.INTEGER
       },
--- db/models/favorite.js
+++ db/models/favorite.js
@@ -2,6 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Favorite = sequelize.define('Favorite', {
     title: DataTypes.STRING,
+    thumbnail: DataTypes.STRING,
     upvotes: DataTypes.INTEGER,
     image: DataTypes.STRING,
     author: DataTypes.STRING
